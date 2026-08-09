7f5cd8b8f0f790cdb66851563af93742f0f933fa
sinsong <literalkernel@outlook.com>
2026-08-10 00:15:52 +0800

    add application icon & fix build.bat

--- shim/build.bat
+++ shim/build.bat
@@ -1,2 +1,4 @@
-cl /nologo /EHsc /DWIN32 /DNDEBUG /D_CONSOLE /SUBSYSTEM:CONSOLE shim.c /Fe:shim-cli.exe
-cl /nologo /EHsc /DWIN32 /DNDEBUG /D_WINDOWS /SUBSYSTEM:WINDOWS shim.c /Fe:shim-gui.exe
+rc app.rc
+rem /link 必须在编译器输入文件的后面
+cl /nologo /EHsc /DWIN32 /DNDEBUG /D_CONSOLE shim.c app.res /Fe:shim-cli.exe /link /SUBSYSTEM:CONSOLE
+cl /nologo /EHsc /DWIN32 /DNDEBUG /D_WINDOWS shim.c app.res /Fe:shim-gui.exe /link /SUBSYSTEM:WINDOWS
